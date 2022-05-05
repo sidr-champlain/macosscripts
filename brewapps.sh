fc6466f94e70388293a71140a75ad558d23f974e
sidr-champlain <sidr.champlain@protonmail.com>
2022-05-05 07:50:26 -0400

    Updated macosscripts repo

---
+++ brewapps.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+# Install Brew Packages
+
+brew install tree
+brew install python
+
+# Install MacOS Applications
+e
+brew install firefox --cask
+brew install virtualbox --cask
+brew install spotify --cask
+brew install pycharm --cask
+
