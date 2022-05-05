df0ea9bc6357e8df7903be8c91e5f639cb6ff7bc
sidr-champlain <sidr.champlain@protonmail.com>
2022-05-05 08:44:39 -0400

    Updated macosscripts Repo

--- brewapps.sh
+++ brewapps.sh
@@ -4,14 +4,53 @@
 
 brew install tree
 brew install python
+brew install git
+brew install gh
+
+# Install Scripting Language
+
+brew install powershell --cask
+brew install php
+brew install perl
+brew install ruby
+
 
 # Update Bash
 
 brew install bash
 
-# Install MacOS Applications
+# Install MacOS Web Browser Applications
 
 brew install firefox --cask
-brew install virtualbox --cask
-brew install spotify --cask
+brew install google-chrome --cask
+brew install microsoft-edge --cask
+brew install brave-browser --cask
+
+# Install MacOS Development Tools
+
 brew install pycharm --cask
+brew install webstorm --cask
+brew install phpstorm --cask
+brew install wireshark --cask
+brew install virtualbox --cask
+
+# Install MacOS Music Applications
+
+brew install spotify --cask
+brew install pandora --cask
+
+# Install Password Manager
+
+brew install bitwarden --cask
+brew install lastpass --cask
+
+# Install MacOS Microsoft Applications
+
+brew install microsoft-word --cask
+brew install microsoft-powerpoint --cask
+brew install microsoft-excel --cask
+
+# Install Storage Services
+
+brew install google-drive --cask
+brew install dropbox --cask
